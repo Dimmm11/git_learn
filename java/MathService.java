2e9c7d83bc9ebff7b4a15e504a062e407a7adb4c
Dmytro Antonov <okpanda11@gmail.com>
2021-09-06 23:05:01 +0300

    added impl for add-3 params & changed message for add-2 params

--- java/MathService.java
+++ java/MathService.java
@@ -3,7 +3,12 @@ import java.util.Optional;
 public class MathService {
 
     public int add(int a, int b) {
-        System.out.println("This is add method");
+        System.out.println("This is add method for 2 params");
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        System.out.println("This is add method for 3 params");
         return a + b;
     }
 
