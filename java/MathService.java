9a564931f514f7d741adc4fe5e500cd99b28e3e6
Dmytro Antonov <okpanda11@gmail.com>
2021-09-06 23:00:04 +0300

    squashed commits from otherOperation branch to dev

--- java/MathService.java
+++ java/MathService.java
@@ -1,6 +1,24 @@
+import java.util.Optional;
+
 public class MathService {
 
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public int division(int a, int b) {
+        return a / b;
+    }
+
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public double sqrt(double a) {
+        return Math.sqrt(a);
     }
 }
